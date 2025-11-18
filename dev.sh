c995e56d590f79b4409ad599157d8e9d405b1623
francisnicholasramos <pfrancisnicholasramos@gmail.com>
2025-11-19 01:44:15 +0800

    add neovim

--- dev.sh
+++ dev.sh
@@ -13,12 +13,13 @@ git clone https://github.com/francisnicholasramos/dotfiles.git $HOME/configDir
 
 mv ~/configDir/nvim ~/.config/
 
-mv ~/configDir/.tmux.conf ~/configDir/.vimrc ~/
+mv ~/configDir/.tmux.conf ~/
 
 cat ~/configDir/.bashrc >> ~/.bashrc
 
 cp -r ~/.config/nvim/colors ~/.vim
 
+echo 'export PATH=/opt/nvim/bin:$PATH' >> ~/.bashrc
 echo 'export PATH=/usr/local/node/bin:$PATH' >> ~/.bashrc
 
 echo ""
@@ -34,6 +35,18 @@ curl -O https://nodejs.org/dist/v24.11.1/node-v24.11.1-linux-x64.tar.xz
 tar -xf node-v24.11.1-linux-x64.tar.xz
 
 sudo mv node-v24.11.1-linux-x64 /usr/local/node
+
+echo ""
+echo "========================="
+echo " Installing NeoVim..."
+echo "========================="
+echo ""
+
+curl -LO https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-linux-x86_64.tar.gz
+
+tar -xzf nvim-linux-x86_64.tar.gz
+
+sudo mv nvim-linux-x86_64 /opt/nvim
 
 echo ""
 echo "======================================="
